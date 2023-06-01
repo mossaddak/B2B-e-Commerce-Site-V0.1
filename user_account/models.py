e4f360e422fd06bf39856f48c3766187e56cc2f6
mossaddak <mossaddak15-2413@diu.edu.bd>
2023-06-01 22:53:37 +0600

    user model updated

--- user_account/models.py
+++ user_account/models.py
@@ -5,11 +5,18 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    ACCOUNT_TYPE_CHOICES = [
+        ('merchant', 'Merchant')
+    ]
+
+
     email = models.EmailField(max_length=50, unique=True, error_messages={"unique":"A user with that email already exists."})
     is_verified = models.BooleanField(default=False)
     otp = models.CharField(max_length=20, null=True, blank=True)
     password_reset_token = models.CharField(max_length=20, null=True, blank=True)
     is_subscribed = models.BooleanField(default=False)
+
+    account_type = models.CharField(max_length=50, choices=ACCOUNT_TYPE_CHOICES, null=True, blank=True, default="merchant")
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['first_name', 'last_name', 'username']
     objects = CustomeUserManager()
@@ -18,7 +25,7 @@ class User(AbstractUser):
         return f"{self.pk}.{self.username}"
     
     class Meta:
-        verbose_name_plural = 'Marchant'
+        verbose_name_plural = 'Merchant'
     
 class ProfilePicture(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="profile_picture")
