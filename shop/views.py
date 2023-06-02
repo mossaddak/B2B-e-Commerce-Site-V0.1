37967450cb265093210be513cd6e04f460b32d2d
mossaddak <mossaddak15-2413@diu.edu.bd>
2023-06-02 12:48:20 +0600

    category get method added

--- shop/views.py
+++ shop/views.py
@@ -8,6 +8,9 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializer import (
     ShopCategorySerializer
 )
+from .models import(
+    ShopCategory
+)
 
 
 
@@ -17,22 +20,17 @@ class ShopCategoryView(APIView):
     authentication_classes = [JWTAuthentication]
 
     def post(self, request):
-        
         if request.user.is_superuser:
             data = request.data
             serializer = ShopCategorySerializer(data=data)
-
             if serializer.is_valid():
                 serializer.save()
-
                 return Response(
                     {
                         "data":serializer.data,
                         "message":"Category Created"
                     }, status=status.HTTP_201_CREATED
-                    
                 )
-            
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
@@ -42,4 +40,24 @@ class ShopCategoryView(APIView):
                 "message":"You don't have permissions for this action"
             },status=status.HTTP_400_BAD_REQUEST
             
+        )
+    
+    def get(self, request):
+        
+        if request.user.is_superuser:
+            data = ShopCategory.objects.all()
+            serializer = ShopCategorySerializer(data, many=True)
+
+            return Response(
+                {
+                    "data":serializer.data,
+                    "message":"Data Fetch"
+                }, status=status.HTTP_202_ACCEPTED
+            )
+    
+        return Response(
+            {
+                "data":{},
+                "message":"You don't have permissions for this action"
+            },status=status.HTTP_400_BAD_REQUEST
         )
